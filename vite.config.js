9ec743b170d0cc7f2d5df6937d5b885c6b737b88
om wankhede <omwankhede2002@gmail.com>
2024-03-05 12:25:42 +0530

    change main page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/vite-project/",
+  base: "/ominfo.xyz/",
   build: {
     outDir: "./docs",
   },
